0af4a450ba4477a6424049085c39712baa586dc3
Marc Schier <mschier@microsoft.com>
2019-01-29 17:33:36 +0100

    design

--- src/Microsoft.Azure.IIoT.Core/src/Extensions/XmlQualifiedNameEx.cs
+++ src/Microsoft.Azure.IIoT.Core/src/Extensions/XmlQualifiedNameEx.cs
@@ -13,9 +13,43 @@ namespace System.Xml {
         /// <summary>
         /// Returns whether the name is null or empty string
         /// </summary>
-        /// <param name="qn"></param>
+        /// <param name="qname"></param>
         /// <returns></returns>
-        public static bool IsNullOrEmpty(this XmlQualifiedName qn) =>
-            qn == null || string.IsNullOrEmpty(qn.Name);
+        public static bool IsNullOrEmpty(this XmlQualifiedName qname) =>
+            qname == null || string.IsNullOrEmpty(qname.Name);
+
+        /// <summary>
+        /// Checks whether the qname is valid
+        /// </summary>
+        /// <param name="qname"></param>
+        /// <returns></returns>
+        public static bool IsValid(this XmlQualifiedName qname) {
+            return !qname.IsNullOrEmpty() && !string.IsNullOrEmpty(qname.Namespace) &&
+                IsValidName(qname.Name);
+        }
+
+        /// <summary>
+        /// Checks if a string is a valid part of a qname.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        internal static bool IsValidName(string name) {
+            if (string.IsNullOrEmpty(name)) {
+                return false;
+            }
+            if (!char.IsLetter(name[0]) && name[0] != '_') {
+                return false;
+            }
+            foreach (var c in name) {
+                if (char.IsLetter(c) || char.IsDigit(c)) {
+                    continue;
+                }
+                if (c == '.' || c == '-' || c == '_') {
+                    continue;
+                }
+                return false;
+            }
+            return true;
+        }
     }
 }
